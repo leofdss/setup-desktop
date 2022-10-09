61dfa1ad0a39241489ef5c6d573c3b256354016e
leofdss <leonardo.f.desouzasilva@gmail.com>
2022-10-09 19:44:36 -0300

    refactor(snap): simplification

--- snap/install.sh
+++ snap/install.sh
@@ -5,22 +5,22 @@ set -e # strict mode
 echo 'install snap'
 
 # install ###################
-if ! which snap &> /dev/null; then
-	if which dnf &> /dev/null; then
-		sudo dnf install snapd -y
-		sudo ln -s /var/lib/snapd/snap /snap
-	fi
-	if which apt &> /dev/null; then
-		sudo apt updade
-		sudo apt install snapd -y
-	fi
-	if ! which snap &> /dev/null; then
-		echo 'install snap error!!!'
-		exit 1
-	fi
+if which dnf &>/dev/null; then
+	sudo dnf install snapd -y
+	sudo ln -s /var/lib/snapd/snap /snap
+fi
+if which apt &>/dev/null; then
+	sudo apt updade
+	sudo apt install snapd -y
 fi
 #############################
 
+# check #####################
+if ! which snap &>/dev/null; then
+	echo 'install snap error!!!'
+	exit 1
+fi
+#############################
 
 # exit ######################
 echo 'snap instaled'
