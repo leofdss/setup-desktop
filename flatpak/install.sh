5a48e4c9ef0d1465e01d4d9b1e6020b25703645d
leofdss <leonardo.f.desouzasilva@gmail.com>
2022-10-09 19:43:35 -0300

    refactor(flatpak): simplification

--- flatpak/install.sh
+++ flatpak/install.sh
@@ -5,24 +5,25 @@ set -e # strict mode
 echo 'install flatpak'
 
 # install ###################
-for check in 'flatpak' 'gnome-software'; do
-	if ! which "$check" &>/dev/null; then
-		if which dnf &>/dev/null; then
-			sudo dnf install "$check" -y
-		fi
-		if which apt &>/dev/null; then
-			sudo apt update
-			sudo apt install "$check" -y
-		fi
-		if ! which $check &>/dev/null; then
-			echo "install $check error!!!"
-			exit 1
-		fi
-	fi
-done
+if which dnf &>/dev/null; then
+	sudo dnf install flatpak -y
+fi
+if which apt &>/dev/null; then
+	sudo apt update
+	sudo apt install flatpak -y
+fi
 #############################
 
+# check #####################
+if ! which flatpak &>/dev/null; then
+	echo 'install flatpak error!!!'
+	exit 1
+fi
+#############################
+
+# config ####################
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#############################
 
 # exit ######################
 echo 'flatpak instaled'
