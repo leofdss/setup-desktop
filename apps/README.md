c41850b2add5dd216b814de541734eb8f3f70ff5
leofdss <leonardo.f.desouzasilva@gmail.com>
2022-10-12 18:46:42 -0300

    feat(apps): install apps by flathub

---
+++ apps/README.md
@@ -0,0 +1,5 @@
+# Apps
+
+```bash
+cat ./database.json | bash ./install.sh
+```
---
+++ apps/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e # strict mode
+
+declare -a A=("$@")
+[[ -p /dev/stdin ]] && { mapfile -t -O ${#A[@]} A; set -- "${A[@]}"; }
+
+echo "$@" | jq -r '.flatpak.flathub[]' | while read object; do
+  flatpak install flathub $object -y --noninteractive
+done
